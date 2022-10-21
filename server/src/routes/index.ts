7cc2ca83710503e2d9ebdcbd9113779d553dd6e2
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2022-10-21 14:19:52 -0700

    build: add types for bcrypt
    move some types dependencies to dev dependencies

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import registerRouer from './auth/registerRoute';
+import loginRouter from './auth/loginRoute';
 
 const router = express.Router();
 
 router.use('/', registerRouer);
+router.use('/', loginRouter);
 
 export default router;
