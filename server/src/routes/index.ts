dc6d9aa3bc8d0476328fa60d843f3869db1fc590
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2022-10-22 05:25:22 -0700

    feat: add auth middleware for protected routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,6 +3,7 @@ import registerRoute from './auth/registerRoute';
 import loginRoute from './auth/loginRoute';
 import refreshRoute from './auth/refreshRoute';
 import logoutRoute from './auth/logoutRoute';
+import { authMiddleware } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
@@ -10,5 +11,8 @@ router.use('/', registerRoute);
 router.use('/', loginRoute);
 router.use('/', refreshRoute);
 router.use('/', logoutRoute);
+
+// routes added after this middleware requires authentication (access token)
+router.use(authMiddleware);
 
 export default router;
